86a213e8dabaa7e3f06a4fdcfd0fec7a5e8c8de8
Lena <131879516+Reistra@users.noreply.github.com>
2026-04-16 07:24:35 +0300

    Create glossaryHandler.go

---
+++ back/internal/handlers/glossaryHandler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"ConsultantBack/internal/db/domain"
+	"context"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type dataGlossary struct {
+	States []domain.State
+	Active string
+	Title  string
+}
+
+func GlossaryHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Got request on glossary page")
+
+	tmpl, err := template.ParseFiles("/app/front/glossary.html", "/app/front/base.html")
+
+	if err != nil {
+		fmt.Printf("Error while templating: %s\n", err)
+	}
+
+	states, err := domain.StatePRepo.GetList(context.Background(), "/consumer/glossary")
+	if err != nil {
+		fmt.Printf("Error while bd getting: %s\n", err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base",
+		dataGlossary{
+			States: states,
+			Active: "glossary",
+			Title:  "Глоссарий ЖКХ",
+		})
+
+	if err != nil {
+		fmt.Printf("Error while templating: %s\n", err)
+	}
+
+	//http.ServeFile(w, req, "./static/mainPage/index.html")
+}
