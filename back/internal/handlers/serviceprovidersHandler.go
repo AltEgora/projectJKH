384be0612e2cb2d51c9118d7af32b5f0b93e9dc4
Lena <131879516+Reistra@users.noreply.github.com>
2026-04-16 05:43:12 +0300

    create serviceprovidersHandler

---
+++ back/internal/handlers/serviceprovidersHandler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"ConsultantBack/internal/db/domain"
+	"context"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type dataEpd struct {
+	States []domain.State
+	Active string
+	Title  string
+}
+
+func EpdHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Got request on serviceproviders page")
+
+	tmpl, err := template.ParseFiles("/app/front/serviceproviders.html", "/app/front/base.html")
+
+	if err != nil {
+		fmt.Printf("Error while templating: %s\n", err)
+	}
+
+	states, err := domain.StatePRepo.GetList(context.Background(), "/consumer/serviceproviders")
+	if err != nil {
+		fmt.Printf("Error while bd getting: %s\n", err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base",
+		dataEpd{
+			States: states,
+			Active: "serviceproviders",
+			Title:  "Поставщики услуг и органы контроля",
+		})
+
+	if err != nil {
+		fmt.Printf("Error while templating: %s\n", err)
+	}
+
+	//http.ServeFile(w, req, "./static/mainPage/index.html")
+}
